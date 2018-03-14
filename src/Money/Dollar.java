6bb134e34528cb4169fab701336016d6194e5046
kassayAkos <kassay.akos@minicrm.hu>
2018-03-15 01:19:04 +0200

    Chapter 3

--- src/Money/Dollar.java
+++ src/Money/Dollar.java
@@ -10,4 +10,9 @@ public class Dollar {
     Dollar times(int multiplayer) {
         return new Dollar(amount * multiplayer);
     }
+    
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar)object;
+        return this.amount == dollar.amount;
+    }
 }
